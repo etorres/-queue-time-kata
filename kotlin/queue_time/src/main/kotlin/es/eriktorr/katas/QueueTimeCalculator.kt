653b458cfa248e300451c9180c255d78e90a45fb
Erik Torres <erik.torres@codurance.com>
2017-12-08 00:26:27 +0100

    completed kotlin version

--- kotlin/queue_time/src/main/kotlin/es/eriktorr/katas/QueueTimeCalculator.kt
+++ kotlin/queue_time/src/main/kotlin/es/eriktorr/katas/QueueTimeCalculator.kt
@@ -1,9 +1,40 @@
 package es.eriktorr.katas
 
+import java.util.*
+
 class QueueTimeCalculator {
 
     fun queueTime(queue: List<Int>, tillsCount: Int): Int {
-        TODO("not implemented")
+        val deque = ArrayDeque<Int>(queue)
+        val tills = generateSequence(0) { it + 1 }.take(tillsCount)
+                .map { _ -> aSelfCheckoutTill(deque)}
+                .toList()
+
+        var queueTime = 0
+
+        do {
+            val minQueueTime = tills.minBy { it.waitingTime }!!.waitingTime
+            queueTime += minQueueTime
+
+            tills.forEach { it ->
+                it.progress(minQueueTime)
+                if (it.waitingTime == 0 && !deque.isEmpty()) it.waitingTime = deque.remove()
+            }
+        } while (!deque.isEmpty())
+
+        queueTime += tills.maxBy { it.waitingTime }!!.waitingTime
+
+        return queueTime
+    }
+
+    private fun aSelfCheckoutTill(deque: ArrayDeque<Int>): SelfCheckoutTill {
+        return SelfCheckoutTill(if (!deque.isEmpty()) deque.remove() else 0)
+    }
+
+    private data class SelfCheckoutTill(var waitingTime: Int = 0) {
+        fun progress(value: Int) {
+            waitingTime -= value
+        }
     }
 
 }
